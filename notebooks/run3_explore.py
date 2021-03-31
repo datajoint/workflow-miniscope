fe8ea5568f199ce3580850b1bf54b27e12892e5f
Kabilar Gunalan <kabilar@vathes.com>
2021-03-31 17:39:02 -0500

    Add `jupytext` support

---
+++ notebooks/run3_explore.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.1
+#   kernelspec:
+#     display_name: 'Python 3.7.9 64-bit (''workflow-imaging'': conda)'
+#     metadata:
+#       interpreter:
+#         hash: 134d995680d44ce2483a761d95a16e9ce77f34191f18929365aa0ab3279667a1
+#     name: python3
+# ---
+
+# # DataJoint U24 Workflow Imaging
+# This notebook will describe the steps for interacting with the data ingested into `workflow-miniscope`.  
+#
+# Prior to using this notebook, please refer to the [README](https://github.com/datajoint/workflow-imaging) for the topics listed below.  
+#     + Installation instructions  
+#     + Directory structure and file naming convention  
+#     + Running the workflow  
+
+import os
+os.chdir('..')
+from workflow_imaging.pipeline import *
+
+# ## Workflow architecture
+
+dj.Diagram(lab)
+
+dj.Diagram(subject)
+
+dj.Diagram(scan)
+
+dj.Diagram(imaging)
+
+subject.Subject()
+
+Session()
+
+scan.ScanInfo()
+
+scan.ScanInfo.Field()
+
+imaging.ProcessingParamSet()
+
+imaging.ProcessingTask()
+
+
---
+++ notebooks/run5_drop.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Drop schemas
+#
+# + This notebook is NOT required.
+# + If required to drop all schemas, the following is the dependency order.
+
+from workflow_miniscope.pipeline import *
+
+# +
+# imaging.schema.drop()
+# scan.schema.drop()
+# session.schema.drop()
+# subject.schema.drop()
+# lab.schema.drop()

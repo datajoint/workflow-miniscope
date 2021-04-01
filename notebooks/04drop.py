1abbcfbfdd8555e005d0d312711a2b10984897d9
Kabilar Gunalan <kabilar@vathes.com>
2021-04-01 03:11:44 -0500

    Update 04drop notebook

--- notebooks/04drop.py
+++ notebooks/04drop.py
@@ -8,8 +8,10 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.11.1
 #   kernelspec:
-#     display_name: Python 3
-#     language: python
+#     display_name: 'Python 3.7.10 64-bit (''workflow-miniscope'': conda)'
+#     metadata:
+#       interpreter:
+#         hash: fb69438121b2de1073146525f02c3c959a56b9d2c1d6b0a62c34f01201323d1c
 #     name: python3
 # ---
 
@@ -18,6 +20,8 @@
 # + This notebook is NOT required.
 # + If required to drop all schemas, the following is the dependency order.
 
+import os
+os.chdir('..')
 from workflow_miniscope.pipeline import *
 
 # +
@@ -26,3 +30,6 @@ from workflow_miniscope.pipeline import *
 # session.schema.drop()
 # subject.schema.drop()
 # lab.schema.drop()
+# -
+
+

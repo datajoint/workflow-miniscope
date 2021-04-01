c991d5d2b54c42427109c61b2867db9351ddd309
Kabilar Gunalan <kabilar@vathes.com>
2021-04-01 03:19:35 -0500

    Update 03automate notebook

--- notebooks/03automate.py
+++ notebooks/03automate.py
@@ -7,17 +7,30 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.11.1
+#   kernelspec:
+#     display_name: 'Python 3.7.10 64-bit (''workflow-miniscope'': conda)'
+#     metadata:
+#       interpreter:
+#         hash: fb69438121b2de1073146525f02c3c959a56b9d2c1d6b0a62c34f01201323d1c
+#     name: python3
 # ---
 
-# or with the `ingest` method and accompanying `csv` files.
-
-# # Automated workflow
+# # [WIP] Automated workflow
 # ### Method for inserting entries
 #
 # Modify `user_data/subjects.csv` and `user_data/sessions.csv`, and run the following commands
+#
+# or with the `ingest` method and accompanying `csv` files.
 
+#
+
+import os
+os.chdir('..')
+from workflow_miniscope.pipeline import *
 from workflow_miniscope.ingest import ingest_subjects, ingest_sessions
+
 ingest_subjects()
+
 ingest_sessions()
 
 # +

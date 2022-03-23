0be82003a2ce61c8dace68faa1b32fba14aad1c2
Kabilar Gunalan <kabilar@datajoint.com>
2022-03-22 22:58:44 -0500

    Update author and description

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ pkg_name = 'workflow_miniscope'
 here = path.abspath(path.dirname(__file__))
 
 long_description = """"
-# Workflow for miniscope calcium imaging data acquired with Miniscope-DAQ-V3 software and analyzed with MiniscopeAnalysis.
+# Workflow for miniscope calcium imaging data acquired with Miniscope-DAQ-V3/V4 software and analyzed with MiniAn.
 
 Build a complete imaging workflow using the DataJoint elements
-+ [elements-lab](https://github.com/datajoint/elements-lab)
-+ [elements-animal](https://github.com/datajoint/elements-animal)
++ [element-lab](https://github.com/datajoint/element-lab)
++ [element-animal](https://github.com/datajoint/element-animal)
++ [element-session](https://github.com/datajoint/element-session)
++ [element-event](https://github.com/datajoint/element-event)
 + [element-miniscope](https://github.com/datajoint/element-miniscope)
 """
 
@@ -27,8 +29,8 @@ setup(
     description="Miniscope calcium imaging workflow using the DataJoint elements",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='DataJoint NEURO',
-    author_email='info@vathes.com',
+    author='DataJoint',
+    author_email='info@datajoint.com',
     license='MIT',
     url='https://github.com/datajoint/workflow-miniscope',
     keywords='neuroscience datajoint calcium-imaging miniscope',

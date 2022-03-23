8477ac3f87bcf8131f64234f6ac3ff98261438c0
Kabilar Gunalan <kabilar@datajoint.com>
2022-03-22 23:38:34 -0500

    Fix for `element-session` updates

--- workflow_miniscope/pipeline.py
+++ workflow_miniscope/pipeline.py
@@ -1,12 +1,12 @@
 import datajoint as dj
 from element_lab import lab
 from element_animal import subject
-from element_session import session
+from element_session import session_with_datetime as session
 from element_miniscope import miniscope
 
 from element_lab.lab import Source, Lab, Protocol, User, Location, Project
 from element_animal.subject import Subject
-from element_session.session import Session
+from element_session.session_with_datetime import Session
 
 from .paths import get_miniscope_root_data_dir
 
@@ -17,16 +17,17 @@ if 'custom' not in dj.config:
 db_prefix = dj.config['custom'].get('database.prefix', '')
 
 
-# Activate `lab`, `subject`, `session` schema ----------------------------------
+# Activate `lab`, `subject`, `session` schema ------------------------------------------
 
 lab.activate(db_prefix + 'lab')
 
 subject.activate(db_prefix + 'subject', linking_module=__name__)
 
+Experimenter = lab.User
 session.activate(db_prefix + 'session', linking_module=__name__)
 
 
-# Declare table `Equipment` for use in element_miniscope -----------------------
+# Declare table `Equipment` for use in element_miniscope -------------------------------
 
 @lab.schema
 class Equipment(dj.Manual):
@@ -35,6 +36,6 @@ class Equipment(dj.Manual):
     """
 
 
-# Activate `miniscope` schema --------------------------------------------------
+# Activate `miniscope` schema ----------------------------------------------------------
 
 miniscope.activate(db_prefix + 'miniscope',  linking_module=__name__)

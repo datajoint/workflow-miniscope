1cd7bfbc2970c1e0e19ed924e12c56a618a1b6e6
Kabilar Gunalan <kabilar@datajoint.com>
2022-03-23 10:41:14 -0500

    Add `get_session_directory`

--- workflow_miniscope/paths.py
+++ workflow_miniscope/paths.py
@@ -2,7 +2,12 @@ import datajoint as dj
 
 
 def get_miniscope_root_data_dir():
-    root_data_dirs = dj.config.get('custom', {}).get('miniscope_root_data_dir', None)
+    return dj.config.get('custom', {}).get('miniscope_root_data_dir', None)
 
-    return root_data_dirs
+
+def get_session_directory(session_key: dict) -> str:
+    from .pipeline import session
+    session_dir = (session.SessionDirectory & session_key).fetch1('session_dir')
+    return session_dir
+
 

db78117f739d2bf28e01bfcb846f095ec23819b6
Kabilar Gunalan <kabilar@datajoint.com>
2022-04-26 04:22:33 +0000

    Update paths

--- workflow_miniscope/paths.py
+++ workflow_miniscope/paths.py
@@ -9,5 +9,3 @@ def get_session_directory(session_key: dict) -> str:
     from .pipeline import session
     session_dir = (session.SessionDirectory & session_key).fetch1('session_dir')
     return session_dir
-
-

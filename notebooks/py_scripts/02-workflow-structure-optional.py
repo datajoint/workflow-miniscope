fd89eba629efabe85ec8c3bd3b76d040e168632c
Chris Broz <CBroz@datajoint.com>
2022-07-12 10:11:00 -0500

    Revise notebook 02 - formatting issues

--- notebooks/py_scripts/02-workflow-structure-optional.py
+++ notebooks/py_scripts/02-workflow-structure-optional.py
@@ -25,7 +25,7 @@
 # To load the local configuration, we will change the directory to the package root.
 
 import os
-if os.path.basename(os.getcwd())=='notebooks': os.chdir('..')
+os.chdir('..')
 
 # ## Schemas and tables
 #
@@ -36,30 +36,27 @@ from workflow_miniscope.pipeline import lab, subject, session, miniscope
 
 # + Each module contains a schema object that enables interaction with the schema in the database.
 
-
 # + Each module imported above corresponds to one schema inside the database. For example, `ephys` corresponds to `neuro_ephys` schema in the database.
 miniscope.schema
 
 # + The table classes in the module corresponds to a table in the schema in the database.
-
 
 # + Each datajoint table class inside the module corresponds to a table inside the schema. For example, the class `ephys.EphysRecording` correponds to the table `_ephys_recording` in the schema `neuro_ephys` in the database.
 # preview columns and contents in a table
 miniscope.Processing()
 
 # + The first time importing the modules, empty schemas and tables will be created in the database. [markdown]
-# # # + By importing the modules for the first time, the schemas and tables will be created inside the database.
+# # + By importing the modules for the first time, the schemas and tables will be created inside the database.
 #
-# # # + Once created, importing modules will not create schemas and tables again, but the existing schemas/tables can be accessed and manipulated by the modules.
+# # + Once created, importing modules will not create schemas and tables again, but the existing schemas/tables can be accessed and manipulated by the modules.
 # + The schemas and tables will not be re-created when importing modules if they have existed. [markdown]
 # ## DataJoint tools to explore schemas and tables
 #
-# # # + `dj.list_schemas()`: list all schemas a user has access to in the current database
+# # + `dj.list_schemas()`: list all schemas a user has access to in the current database
 # + `dj.list_schemas()`: list all schemas a user could access.
 dj.list_schemas()
 
-# + `dj.Diagram()`: plot tables and dependencies in a schema.
-
+# + `dj.Diagram()`: plot tables and dependencies in a schema. 
 
 # + `dj.Diagram()`: plot tables and dependencies
 # plot diagram for all tables in a schema
@@ -109,12 +106,11 @@ dj.Diagram(subject) + dj.Diagram(session) + dj.Diagram(miniscope)
 dj.Diagram(subject.Subject) + dj.Diagram(session.Session) + dj.Diagram(miniscope)
 
 # + `heading`: [markdown]
-# # # + `describe()`: show table definition with foreign key references.
+# # + `describe()`: show table definition with foreign key references.
 # -
 miniscope.Processing.describe();
 
 # + `heading`: show attribute definitions regardless of foreign key references
-
 
 # + `heading`: show table attributes regardless of foreign key references.
 miniscope.Processing.heading
@@ -122,7 +118,7 @@ miniscope.Processing.heading
 # + ephys [markdown]
 # # DataJoint Elements installed in `workflow-miniscope`
 #
-# # # + [`lab`](https://github.com/datajoint/element-lab): lab management related information, such as Lab, User, Project, Protocol, Source.
+# # + [`lab`](https://github.com/datajoint/element-lab): lab management related information, such as Lab, User, Project, Protocol, Source.
 # -
 
 dj.Diagram(lab)
@@ -142,7 +138,6 @@ dj.Diagram(session)
 session.Session.describe();
 
 # + [`miniscope`](https://github.com/datajoint/element-miniscope): miniscope raw recording and processed data
-
 
 # + [probe and ephys](https://github.com/datajoint/element-array-ephys): Neuropixel based probe and ephys tables
 dj.Diagram(miniscope)

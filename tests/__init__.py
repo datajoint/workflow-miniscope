68fb4a615006ece4d684ceac98044be2a629c48b
Kabilar Gunalan <kabilar@datajoint.com>
2023-05-17 18:24:43 -0500

    Update pytest init

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+run all: python -m pytest -sv --cov-report term-missing --cov=workflow_miniscope --sw -p no:warnings tests/
+run one: python -m pytest -sv --cov-report term-missing --cov=workflow_miniscope --sw -p no:warnings --pdb tests/module_name.py -k function_name
+"""
